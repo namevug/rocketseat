57f6cc7831621b6222378f0955c61774b31347bb
namevug <ciavug@gmail.com>
2022-02-24 16:47:34 -0300

    feat: rocketseat-js

---
+++ guia-estelar-de-JavaScript/scripts.js
@@ -0,0 +1,23 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+//console.log(person.name)
+
+
+
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat',
+]
+
+// acessar valores dentro do Array
+console.log(animals[1])
